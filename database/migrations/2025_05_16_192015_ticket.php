b18cf14fc4448177f698ccc4468cc257f76c1cda
Roma Volkov <r.volkov@yaware.com>
2025-05-17 21:02:57 +0300

    add ticket

---
+++ database/migrations/2025_05_16_192015_ticket.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+       public function up()
+    {
+        Schema::create('ticket_tickets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('subject');
+            $table->text('content');
+            $table->string('status', 16);
+            $table->timestamps();
+        });
+
+        Schema::create('ticket_statuses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ticket_id')->references('id')->on('ticket_tickets')->onDelete('cascade');
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('status', 16);
+            $table->timestamps();
+        });
+
+        Schema::create('ticket_messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('ticket_id')->references('id')->on('ticket_tickets')->onDelete('cascade');
+            $table->integer('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('ticket_messages');
+        Schema::dropIfExists('ticket_statuses');
+        Schema::dropIfExists('ticket_tickets');
+    }
+};
